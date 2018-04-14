f145705c8032947bb0b6f34c2e0540a58d11524d
kingcos <2821836721v@gmail.com>
2018-04-15 04:09:58 +0800

    Add Helper

---
+++ ARGallery/Helper.swift
@@ -0,0 +1,26 @@
+//
+//  Helper.swift
+//  ARGallery
+//
+//  Created by kingcos on 2018/4/14.
+//  Copyright © 2018 kingcos. All rights reserved.
+//
+
+import UIKit
+
+class Helper {
+    
+}
+
+// API
+extension Helper {
+    static let styleImages = [
+        "FanGaoXingKong": "https://i.loli.net/2018/04/14/5ad1da4b774ed.png",
+        "DaFenQi": "",
+        "GuoHua": ""
+    ]
+    
+    static let exampleImages = [
+        "https://i.loli.net/2018/04/14/5ad1da6b5f355.jpg"
+    ]
+}
